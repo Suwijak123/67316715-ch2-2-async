998dce475d3023dee3f16538ce1255228eb18551
Suwijak123 <1suwijakt@gmail.com>
2026-07-01 10:51:32 +0700

    1

--- ab2_exercise3.dart
+++ ab2_exercise3.dart
@@ -3,8 +3,22 @@ void main() {
   List<String> subjects = ['Cal', 'Phy', 'Comarh'];
   print('number of subjects: ${subjects.length}');
   print('first subject: ${subjects[0]}');
-  print('last subject : ${subjects.last}');
+  print('last subject: ${subjects.last}');
 
   subjects.add('Eng');
+  print('updated subject: ${subjects}');
+
+  Map<String, int>studentscore ={
+    'Cal':75 ,
+    'Phy':85,
+    'Comarh':89
+  };
+  print('Score for Cal: ${studentscore['Cal']}');
+
+  studentscore['Eng'] = 80;
+  print('updated: $studentscore');
+  print('All subjects in map: ${studentscore.keys}');
+  print('All scores in map: ${studentscore.values}');
+
 }
 
